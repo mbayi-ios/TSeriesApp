d3cacfa8ad3298bea3819e137da3946f2c015277
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-01-22 13:31:31 +0300

    Initial Commit

---
+++ TSeriesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TSeriesApp
+//
+//  Created by Amby on 22/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TSeriesApp/TSeriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TSeriesAppApp.swift
+//  TSeriesApp
+//
+//  Created by Amby on 22/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TSeriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
